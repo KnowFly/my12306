24a6cf5750356f15eee06988b5e88f1692e0be80
dongfei <dongfei@rd-goodnovel.com>
2022-09-29 20:43:57 +0800

    requests-html

--- core/requestHtml.py
+++ core/requestHtml.py
@@ -1,4 +1,4 @@
-import requests
+# import requests
 from requests_html import HTMLSession
 from url_cons import login_url
 
@@ -8,30 +8,28 @@ headers = {
                   "Chrome/54.0.2840.99 Safari/537.36"}
 
 
-def initRequest():
-    bd_html = requests.get(login_url, params="", headers=headers)
-    if bd_html.status_code == 200:
-        print(bd_html.text)
-        pass
-    else:
-        print("请求失败了")
-        pass
+# def initRequest():
+#     bd_html = requests.get(login_url, params="", headers=headers)
+#     if bd_html.status_code == 200:
+#         print(bd_html.text)
+#         pass
+#     else:
+#         print("请求失败了")
+#         pass
 
 
 def initRequestSession():
-    # 1. 创建session对象，可以保存Cookie值
-    ssion = requests.session()
+    ssion = HTMLSession
     # 2. 处理 headers
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
     # 3. 需要登录的用户名和密码
     data = {"email": "mr_mao_hacker@163.com", "password": "alarmchime"}
-
-    # 4. 发送附带用户名和密码的请求，并获取登录后的Cookie值，保存在ssion里
-    ssion.post("http://www.renren.com/PLogin.do", data=data, headers=headers)
-    # 5. ssion包含用户登录后的Cookie值，可以直接访问那些登录后才可以访问的页面
-    response = ssion.get("http://www.renren.com/410043129/profile")
-
-    # 6. 打印响应内容
-    print
-    response.text
+    session = HTMLSession()
+    # 往京东主页发送get请求
+    jd = session.get('https://jd.com/')
+    # 得到京东主页所有的链接，返回的是一个set集合
+    print(jd.html.links)
+    print('*' * 1000)
+    # 若获取的链接中有相对路径，我们还可以通过absolute_links获取所有绝对链接
+    print(jd.html.absolute_links)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from core.requestHtml import initRequest, initRequestSession
+from core.requestHtml import initRequestSession
 
 if __name__ == '__main__':
     print("开始了")
-    initRequest()
-    # initRequestSession()
+    # initRequest()
+    initRequestSession()
