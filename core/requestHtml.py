c2c1e775151212a3ccabb9e2a478a9c292d0b40c
dongfei <dongfei@rd-goodnovel.com>
2022-09-30 19:23:36 +0800

    增加json模块，解析Json

--- core/requestHtml.py
+++ core/requestHtml.py
@@ -8,13 +8,15 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/54.0.2840.99 Safari/537.36"}
 
+
 def initRequestSession():
     # 3. 需要登录的用户名和密码
-    data = {"username": "17600000000", "appid": "otn"}
+    data = {"username": "17600000000", "appid": "otn", "slideMode": "1"}
     sson = HTMLSession()
-    my12306 = sson.post('https://kyfw.12306.cn/passport/web/checkLoginVerify/', data=data,headers=headers)
+    my12306 = sson.post('https://kyfw.12306.cn/passport/web/checkLoginVerify/', data=data)
+    # my12306 = sson.post('https://kyfw.12306.cn/passport/web/slide-passcode', data=data)
     if my12306.status_code == 200:
-        print("返回数据" + my12306.text)
+        # print("返回数据" + my12306.json())
         # python原始数据
         print("python串" + (repr(my12306.json())))
         # 转换成json字符串
@@ -23,6 +25,9 @@ def initRequestSession():
         # 转化Json字典
         jsonMap = json.loads(jsonStr)
         # 获取字典Value
-        print(jsonMap['login_check_code'])
+        # print(jsonMap['login_check_code'])
+        # print(jsonMap['result_message'])
+        # print(jsonMap['if_check_slide_passcode_token'])
+        # print(jsonMap['result_code'])
     else:
         print("获取验证码失败")
