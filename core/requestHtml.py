a936e3e6d11e161f24930bb00b18d137dff1520b
dongfei <dongfei@rd-goodnovel.com>
2022-10-09 17:41:16 +0800

    增加备注方便学习

--- core/requestHtml.py
+++ core/requestHtml.py
@@ -10,24 +10,49 @@ headers = {
 
 
 def initRequestSession():
-    # 3. 需要登录的用户名和密码
+    # 域名/robots.txt 查看协议
+    # 需要登录的用户名和密码
     data = {"username": "17600000000", "appid": "otn", "slideMode": "1"}
     sson = HTMLSession()
-    my12306 = sson.post('https://kyfw.12306.cn/passport/web/checkLoginVerify/', data=data)
-    # my12306 = sson.post('https://kyfw.12306.cn/passport/web/slide-passcode', data=data)
+    my12306 = sson.post('https://kyfw.12306.cn/passport/web/checkLoginVerify/', headers=headers, data=data)
     if my12306.status_code == 200:
-        # print("返回数据" + my12306.json())
-        # python原始数据
-        print("python串" + (repr(my12306.json())))
-        # 转换成json字符串
-        jsonStr = json.dumps(my12306.json())
-        print("json字符串" + jsonStr)
+        print("获取验证码成功")
+        # response.text 获取字符串类型的响应正文：
+        # response.content 获取bytes类型的响应正文：
+        # response.json()  直接将响应内容的json字符串转换成python的list或者dict
+
+        # json_str = json.dumps(json_data)  # 返回值为json字符串
+        # json.loads(json_str)  # 返回值为python的list或者dict
+
+        json_data = my12306.json()
+        json_str = my12306.text
+        json_content = my12306.content
+
+        print("json字符串" + json_str)  # {"result_message":"","login_check_code":"1","result_code":0}
+        print("============================================")
+        print(json_content)  # b'{"result_message":"","login_check_code":"1","result_code":0}'
+        print("============================================")
+        print(json_data)  # {'result_message': '', 'login_check_code': '1', 'result_code': 0}
+        print("============================================")
+        print(type(json_str))  # <class 'str'>
+        print("============================================")
+        print(type(json_data))  # <class 'dict'>
+        print("============================================")
+        login_check_code = json_data['login_check_code']
+        print(login_check_code)
+        print("============================================")
+        json_str = json.dumps(json_data)
+        print(json_str)  # {"result_message": "", "login_check_code": "1", "result_code": 0}
+        print("============================================")
         # 转化Json字典
-        jsonMap = json.loads(jsonStr)
+        json_map = json.loads(json_str)
         # 获取字典Value
-        # print(jsonMap['login_check_code'])
-        # print(jsonMap['result_message'])
-        # print(jsonMap['if_check_slide_passcode_token'])
-        # print(jsonMap['result_code'])
+        print(json_map['login_check_code'])
+        # 写入Json文件
+        fp = open('./12306_main.json', 'w', encoding='utf-8')  # 创建⽂件
+        json.dump(json_data, fp=fp, ensure_ascii=False)
+        # 写入html文件
+        with open("12306_main.html", mode="w", encoding="utf-8") as f:  # 创建⽂件
+            f.write(json_str)  # 保存在⽂件中
     else:
         print("获取验证码失败")
