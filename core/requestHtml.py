91fd288c8e0f73c5778e23774ce3c85d832249a9
dongfei <dongfei@rd-goodnovel.com>
2022-10-09 20:33:33 +0800

    二维码登录

--- core/requestHtml.py
+++ core/requestHtml.py
@@ -1,6 +1,9 @@
 # import requests
+import base64
+
 from requests_html import HTMLSession
 import json
+from PIL import Image  # 操作图片
 from url_cons import login_url
 
 # 设置请求头
@@ -12,9 +15,12 @@ headers = {
 def initRequestSession():
     # 域名/robots.txt 查看协议
     # 需要登录的用户名和密码
-    data = {"username": "17600000000", "appid": "otn", "slideMode": "1"}
+    data = {"appid": "otn"}
     sson = HTMLSession()
-    my12306 = sson.post('https://kyfw.12306.cn/passport/web/checkLoginVerify/', headers=headers, data=data)
+    cookies = sson.cookies
+    # my12306 = sson.post('https://kyfw.12306.cn/passport/web/checkLoginVerify/', cookies=cookies, headers=headers,
+    my12306 = sson.post('https://kyfw.12306.cn/passport/web/create-qr64', cookies=cookies, headers=headers,
+                        data=data)
     if my12306.status_code == 200:
         print("获取验证码成功")
         # response.text 获取字符串类型的响应正文：
@@ -38,21 +44,38 @@ def initRequestSession():
         print("============================================")
         print(type(json_data))  # <class 'dict'>
         print("============================================")
-        login_check_code = json_data['login_check_code']
-        print(login_check_code)
+        # login_check_code = json_data['login_check_code']
+        # print(login_check_code)
         print("============================================")
         json_str = json.dumps(json_data)
         print(json_str)  # {"result_message": "", "login_check_code": "1", "result_code": 0}
         print("============================================")
         # 转化Json字典
-        json_map = json.loads(json_str)
+        # json_map = json.loads(json_str)
         # 获取字典Value
-        print(json_map['login_check_code'])
+        # print(json_map['login_check_code'])
+
         # 写入Json文件
-        fp = open('./12306_main.json', 'w', encoding='utf-8')  # 创建⽂件
-        json.dump(json_data, fp=fp, ensure_ascii=False)
-        # 写入html文件
-        with open("12306_main.html", mode="w", encoding="utf-8") as f:  # 创建⽂件
-            f.write(json_str)  # 保存在⽂件中
+        # fp = open('./12306_main.json', 'w', encoding='utf-8')  # 创建⽂件
+        # json.dump(json_data, fp=fp, ensure_ascii=False)
+        # # 写入html文件
+        # with open("12306_main.html", mode="w", encoding="utf-8") as f:  # 创建⽂件
+        #     f.write(json_str)  # 保存在⽂件中
+        print("============================================")
+        result_code = json_data['result_code']
+        uuid = json_data['uuid']
+        if result_code == '0':
+            print(json_data['image'])
+            image_base64 = getImage(base64.b64decode(json_data['image']))
+            Image.open(image_base64).show()  # 依赖PIL库，打开图片(会创建一个零食文件打开图片，图片未被占用时销毁)
+        else:
+            print("获取二维码图片失败")
     else:
         print("获取验证码失败")
+
+
+def getImage(img):
+    filepath = './12306_login.jpg'
+    with open(filepath, 'wb') as fd:  # w写入 b二进制形式
+        fd.write(img)
+    return filepath
